2470d417dbdfce428b16e1600abd7e59b73ef928
Frédéric Bolduc <me@ferdbold.com>
2020-08-25 22:47:28 -0400

    Define health properties

--- Source/Beeswax/GameModes/Main/MainPlayerState.cpp
+++ Source/Beeswax/GameModes/Main/MainPlayerState.cpp
@@ -1,2 +1,26 @@
 #include "MainPlayerState.h"
 
+#include "Net/UnrealNetwork.h"
+
+AMainPlayerState::AMainPlayerState()
+{
+	_Health = _MaxHealth = _DefaultMaxHealth;
+}
+
+void AMainPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMainPlayerState, _Health);
+	DOREPLIFETIME(AMainPlayerState, _MaxHealth);
+}
+
+void AMainPlayerState::OnHealthChanged_Implementation()
+{
+
+}
+
+void AMainPlayerState::OnMaxHealthChanged_Implementation()
+{
+
+}
--- Source/Beeswax/GameModes/Main/MainPlayerState.h
+++ Source/Beeswax/GameModes/Main/MainPlayerState.h
@@ -8,5 +8,30 @@ UCLASS()
 class BEESWAX_API AMainPlayerState : public APlayerState
 {
 	GENERATED_BODY()
-	
+
+public:
+	AMainPlayerState();
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE int GetHealth() const { return _Health; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE int GetMaxHealth() const { return _MaxHealth; }
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health", ReplicatedUsing=OnHealthChanged, Meta=(ClampMin=0))
+	int _Health{ 0 };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health", ReplicatedUsing=OnMaxHealthChanged, Meta=(ClampMin=0))
+	int _MaxHealth{ 0 };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health", Meta=(ClampMin=0))
+	int _DefaultMaxHealth{ 0 };
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnHealthChanged();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnMaxHealthChanged();
 };
