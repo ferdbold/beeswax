3a04e9d5c0f10b9bc8693c81266cadf2467f0243
Frédéric Bolduc <me@ferdbold.com>
2020-09-08 22:35:41 -0400

    Basic trap blueprint * Applies damage on contact * Applies repulse on contact

--- Source/Beeswax/GameModes/Main/MainPlayerState.h
+++ Source/Beeswax/GameModes/Main/MainPlayerState.h
@@ -12,11 +12,9 @@ class BEESWAX_API AMainPlayerState : public APlayerState
 public:
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
 
-	UFUNCTION(BlueprintCallable, BlueprintPure)
 	FORCEINLINE int GetHealth() const { return Health; }
 	FORCEINLINE void SetHealth(int value) { Health = value; }
 
-	UFUNCTION(BlueprintCallable, BlueprintPure)
 	FORCEINLINE int GetMaxHealth() const { return MaxHealth; }
 	FORCEINLINE void SetMaxHealth(int value) { MaxHealth = value; }
 
